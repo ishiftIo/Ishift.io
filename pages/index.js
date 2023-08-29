0a4faf6d727f8c863ad34dc1876b85512d009570
ishiftIo <143091000+ishiftIo@users.noreply.github.com>
2023-08-29 18:02:45 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+const handleOnClick = () => {
+    console.log('sdfsdfs');
+    const navELe = document.getElementById('mobile-nav')
+
+    navELe.classList.toggle('open-nav')
+}
+
+ScrollReveal().reveal('.about-container', { delay: 500 });
