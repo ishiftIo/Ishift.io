6f11440f62e0e68953bd6ba536220764b93b0e63
ishiftIo <143091000+ishiftIo@users.noreply.github.com>
2023-08-29 18:00:26 +0530

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-const handleOnClick = () => {
-    console.log('sdfsdfs');
-    const navELe = document.getElementById('mobile-nav')
-
-    navELe.classList.toggle('open-nav')
-}
-
-ScrollReveal().reveal('.about-container', { delay: 500 });
